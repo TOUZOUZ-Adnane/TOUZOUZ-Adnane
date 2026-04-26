54d6b7c10b9884cb2954d112f9cbd5d1d4ef2b0e
TOUZOUZ Adnane <adnane.touzouz.ta@gmail.com>
2026-04-26 20:59:06 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/TOUZOUZ-Adnane",
-  assetPrefix: "/TOUZOUZ-Adnane/",
+  assetPrefix: "/TOUZOUZ-Adnane/out",
   images: {
     unoptimized: true,
   },
