d1bb4545f7fc2e1daf81366361fae59ed535b160
TOUZOUZ Adnane <adnane.touzouz.ta@gmail.com>
2026-05-02 18:01:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "TOUZOUZ-Adnane"; // ← IMPORTANT
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
